2c69dfc5aff43ffa53fc6b5c014a314915417c8e
RAJ PATEL <patelraj24365@gmail.com>
2023-11-23 18:37:28 -0500

    database connected

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,46 +1,48 @@
-import { connectToDB } from '@utils/database'
-import NextAuth from 'next-auth/next'
-import GoogleProvider from 'next-auth/providers/google'
-import User from '@models/users'
-
+import { connectToDB } from "@utils/database";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import User from "@models/users";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId:'686376580444-6gfmje7m097rmn4aegsqqp7ve5jdjfdd.apps.googleusercontent.com',
-            clientSecret:'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'
-        })
-    ],
-    async session({ session }) {
+  providers: [
+    GoogleProvider({
+      clientId:
+        "686376580444-6gfmje7m097rmn4aegsqqp7ve5jdjfdd.apps.googleusercontent.com",
+      clientSecret: "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",
+    }),
+  ],
+    callbacks: {
+      async session({ session }) {
         const sessionUser = await User.findOne({ email: session.user.email });
         session.user.id = sessionUser._id.toString();
-  
+    
         return session;
-
-    },
-    async signIn({ profile }) {
+      },
+      async signIn({ profile }) {
         try {
-            await connectToDB();
+          await connectToDB();
     
-            // check if user already exists
-            const userExists = await User.findOne({ email: profile.email });
+          // check if user already exists
+          const userExists = await User.findOne({ email: profile.email });
     
-            // if not, create a new document and save user in MongoDB
-            if (!userExists) {
-              await User.create({
-                email: profile.email,
-                username: profile.name.replace(" ", "").toLowerCase(),
-                image: profile.picture,
-              });
-            }
-    
-            return true
-          } catch (error) {
-            console.log("Error checking if user exists: ", error.message);
-            return false
+          // if not, create a new document and save user in MongoDB
+          if (!userExists) {
+            await User.create({
+              email: profile.email,
+              username: profile.name.replace(" ", "").toLowerCase(),
+              image: profile.picture,
+            });
           }
+    
+          return true;
+        } catch (error) {
+          console.log("Error checking if user exists: ", error.message);
+          return false;
+        }
+      },
 
-    }
-})
+  }
+  
+});
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
