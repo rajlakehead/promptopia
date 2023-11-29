7d1e4edd09202e6aeab76d031796a2bc6634834e
RAJ PATEL <patelraj24365@gmail.com>
2023-11-28 22:35:44 -0500

    commit

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,15 +1,21 @@
 import Prompt from "@models/prompt";
-import { connectToDB } from "@utils/database";
+import { connectToDatabase } from "@utils/database";
+import { NextRequest, NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
 
-export const revalidate = 1
 export const GET = async (request) => {
-    try {
-        await connectToDB()
+  try {
+    await connectToDatabase();
 
-        const prompts = await Prompt.find({}).populate('creator')
+    const prompts = await Prompt.find({}).populate("creator");
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
-    } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
-    }
-} 
+    //To dynamically get the path
+    const path = request.nextUrl.searchParams.get("path") || "/";
+
+    revalidatePath(path);
+
+    return NextResponse.json(prompts);
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
